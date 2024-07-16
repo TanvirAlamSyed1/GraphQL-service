30beb2cec5f36a4f97bc2b33230c96c65a408442
Tanvir Alam Syed <tanviralamsyed@gmail.com>
2024-07-16 14:35:43 +0100

    This was created using this guide: https://spring.io/guides/gs/graphql-server

---
+++ src/test/java/com/tanvirSyed/GraphQL/service/GraphQlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanvirSyed.GraphQL.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
